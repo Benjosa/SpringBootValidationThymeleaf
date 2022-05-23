102baa7aabed4cc709955e856a76b6e9c5c4fc4c
jonathansablier <jonathansablier@macbook-pro-de-jonathan.home>
2022-05-23 10:12:07 +0200

    Mon premier commit comprenant une classe controller qui appelle une page avec thymeleaf

---
+++ src/test/java/com/inti/SpringSpringBootValidationThymeleaf/SpringBootValidationThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.SpringSpringBootValidationThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
